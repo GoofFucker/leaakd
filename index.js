045b1841cdfd1f3d97a0f141941ec920b6ed6326
GoofFucker <47990107+GoofFucker@users.noreply.github.com>
2019-02-26 19:08:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ client.on("ready", () => {
 
 client.on('guildMemberRemove', member => {
     var ayy2 = client.emojis.find(emoji => emoji.name === "error");
-    member.guild.channels.get('549914111492882442').send(ayy2 + member + " à fait ses adieux. " + ayy2); 
+    member.guild.channels.get('549914111492882442').send(ayy2 + member + " à fait ses adieux. " + ayy2);
+    if (member.guild.id !== serverStats.guildID) return;
+        client.channels.get(serverStats.memberCountID).setName('Membres: ${member.guild.members.filter(m => !n.bot).size}');
+
 });
 
 client.on('guildMemberAdd', member => {
@@ -19,6 +22,8 @@ client.on('guildMemberAdd', member => {
     var ayy = client.emojis.find(emoji => emoji.name === "success");
     member.guild.channels.get('549914111492882442').send(ayy + member + " arrive pour niquer des mères. " + ayy); 
     member.addRole(role)
+    if (member.guild.id !== serverStats.guildID) return;
+        client.channels.get(serverStats.memberCountID).setName('Membres: ${member.guild.members.filter(m => !n.bot).size}');
 });
 
 client.on('message', message => {
