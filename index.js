83235122e6612e49878df4bc8f2a077b78b5591d
GoofFucker <47990107+GoofFucker@users.noreply.github.com>
2019-04-25 16:29:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,6 @@ client.on("ready", () => {
     console.log("Ready");
     client.user.setActivity(`J'ai ${client.guilds.size} serveurs`, { type: "STREAMING", url: "https://www.twitch.tv/gotagatv" })
 })
-
-client.on('guildMemberAdd', member => {
-    member.send('Hey Jeune discordeur\n \n Le discord de la ``SBG`` cherche des membres passioné par le développement\n Nous fournissons des alts et des tools par rapport aux invites.\n \n https://discord.gg/TuD72nN');
-    console.log("SendLeave"); 
-});
-
 client.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
