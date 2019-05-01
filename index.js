0c88379cc86f0f5979adf19a9be005d251749472
GoofFucker <47990107+GoofFucker@users.noreply.github.com>
2019-05-01 16:14:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ client.login(process.env.TOKEN6);
 
 client.on("ready", () => {
     console.log("Ready");
-    client.user.setActivity(`J'ai ${client.guilds.size} serveurs`, { type: "STREAMING", url: "https://www.twitch.tv/gotagatv" })
+    client.user.setActivity(`👑 Roi des Paumés 👑`, { type: "STREAMING", url: "https://www.twitch.tv/gotagatv" })
 })
-client.on("guildCreate", guild => {
-    // This event triggers when the bot joins a guild.
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`J'ai ${client.guilds.size} serveurs`, { type: "STREAMING", url: "https://www.twitch.tv/gotagatv" })
-  });
 
-  client.on("guildDelete", guild => {
-    // This event triggers when the bot joins a guild.
-    console.log(`New guild leave: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`J'ai ${client.guilds.size} serveurs`, { type: "STREAMING", url: "https://www.twitch.tv/gotagatv" })
-  });
